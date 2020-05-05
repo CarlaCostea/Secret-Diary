2ed7590c5c9f51a9bd47896ecb69a80ac05bead1
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-05-05 19:46:31 +0300

    update

---
+++ SecretDiary/RegisterEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretDiary
+{
+        public class RegisterEvent
+        {
+            private DateTime eventTimeStamp;
+            private string eventTitle;
+            private string eventDetails;
+
+            public RegisterEvent(DateTime eventTimeStamp, string eventTitle, string eventDetails)
+            {
+                this.eventTimeStamp = eventTimeStamp;
+                this.eventTitle = eventTitle;
+                this.eventDetails = eventDetails;
+            }
+
+            public string GetEvent()
+            {
+                return eventTimeStamp + "\n" + eventTitle + "\n" + eventDetails;
+            }
+        }
+}
---
+++ SecretDiaryTests/RegisterEventTests.cs
@@ -0,0 +1,19 @@
+﻿using SecretDiary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CircularDoublyLinkedListTest
+{
+    public class RegisterEventTests
+    {
+        [Fact]
+        public void RegisterAnEvent()
+        {
+            RegisterEvent myFirstEvent = new RegisterEvent(DateTime.Now, "New test", "I'm trying to add an event in my Secret Diary");
+            string firstEvent = DateTime.Now + "\n" + "New test" + "\n" + "I'm trying to add an event in my Secret Diary";
+            Assert.Equal(firstEvent, myFirstEvent.GetEvent());
+        }
+    }
+}
